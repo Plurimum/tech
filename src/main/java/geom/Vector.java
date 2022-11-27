016506f39f9a505c4eb4b94ce37616610d4c10e8
Your Name <you@example.com>
2022-11-27 14:11:12 +0300

    line functions

--- src/main/java/geom/Vector.java
+++ src/main/java/geom/Vector.java
@@ -1,4 +1,18 @@
 package geom;
 
 public record Vector(double x, double y) {
+    double qsize() {
+        return x * x + y * y;
+    }
+    double size() {
+        return Math.sqrt(qsize());
+    }
+
+    Vector mul(double t) {
+        return new Vector(x * t, y * t);
+    }
+
+    double dot(Vector other) {
+        return x * other.x + y * other.y;
+    }
 }
