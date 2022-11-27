9db39e775ad448491cdf9473c51def4a72e63667
Maksim Lihanov <maxim.likhanov@vk.team>
2022-11-27 13:23:36 +0300

    read write

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import geom.Point;
+import geom.Rectangle;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (
+                Scanner scanner = new Scanner(Files.newBufferedReader(Path.of("tests/01.txt")));
+                Writer writer = Files.newBufferedWriter(Path.of("out"))
+        ) {
+            ReadWrite readWrite = new ReadWrite();
+            Set<Point> polygon = readWrite.readPolygon(scanner);
+
+            Set<Rectangle> rectangles = Set.of(new Rectangle(new Point(1.2345, 6.789), new Point(6.789, 1.2345))); // get from solve method
+
+            readWrite.writeAnswer(writer, rectangles);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/ReadWrite.java
@@ -0,0 +1,43 @@
+import geom.Point;
+import geom.Rectangle;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class ReadWrite {
+    public Set<Point> readPolygon(Scanner scanner) {
+        String[] firstLine = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(firstLine[0]);
+        Set<Point> polygon = new LinkedHashSet<>();
+
+        polygon.add(new Point(Double.parseDouble(firstLine[1]), Double.parseDouble(firstLine[2])));
+        Stream.generate(() -> {
+                    String[] values = scanner.nextLine().split(" ");
+
+                    return new Point(Double.parseDouble(values[0]), Double.parseDouble(values[1]));
+                })
+                .limit(n - 1)
+                .forEach(polygon::add);
+
+        return polygon;
+    }
+
+    public void writeAnswer(Writer writer, Set<Rectangle> rectangles) throws IOException {
+        writer.write(String.valueOf(rectangles.size()) + '\n');
+
+        rectangles.forEach(rectangle -> {
+            try {
+                Point a = rectangle.a();
+                Point b = rectangle.b();
+
+                writer.write(a.x() + " " + a.y() + " " + b.x() + " " + b.y());
+            } catch (IOException e) {
+                throw new RuntimeException("poh", e);
+            }
+        });
+    }
+}
